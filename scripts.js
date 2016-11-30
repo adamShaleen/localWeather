b0e80d7c91952010f29e2a903227c00804628e4d
adamShaleen <adamshaleen@gmail.com>
2016-11-30 16:37:19 -0600

    changed temp to imperial

--- scripts.js
+++ scripts.js
@@ -9,16 +9,15 @@ var longitude = "";
   });
 
 $("#getWeather").click(function() {
-	$.getJSON("https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&APPID=41a877bfa230ba6c56b60795c0403aa1", function(json) {
+	$.getJSON("https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&APPID=41a877bfa230ba6c56b60795c0403aa1&units=imperial", function(json) {
   	console.log(json);
   	$("#location").html(json.name);
     $("#temp").html(json.main.temp);  // figure out how to set parameters for F and C
     $("#description").html(json.weather[0].description);
   });
 });
-    
+
 });
 
 // latitude 44.741194899999996
 // longitude -92.8635487
-// https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?lat=44.741194899999996&lon=-92.8635487&APPID=41a877bfa230ba6c56b60795c0403aa1
