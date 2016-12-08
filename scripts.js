ed3d5cf99540126f87b8ddc6cf580ebdcbcabc0b
adamShaleen <adamshaleen@gmail.com>
2016-12-07 20:27:22 -0600

    changed API call to ajax and added success function.;

--- scripts.js
+++ scripts.js
@@ -9,14 +9,22 @@ var longitude = "";
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
 
-    // when the API promise is returned, populate the data to respective HTML elements
+    // when the API promise is returned, hide the loading status and populate the data to respective HTML elements
     $("body").promise().done(function() {
-        $.getJSON("http://api.apixu.com/v1/current.json?key=4f7878f8d5bd48de89430958160712&q=" + latitude + "," + longitude, function(json) {
-        $("#location").html(json.location.name + ", " + json.location.region);
-        $("#temp").html(json.current.temp_f + " °F");
-        $("#description").html(json.current.condition.text);
-        $("#icon").attr("src", json.current.condition.icon);
+
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: "http://api.apixu.com/v1/current.json?key=4f7878f8d5bd48de89430958160712&q=" + latitude + "," + longitude,
+            success: function(json) {
+                $("#loading").hide();
+                $("#location").html(json.location.name + ", " + json.location.region);
+                $("#temp").html(json.current.temp_f + " °F");
+                $("#description").html(json.current.condition.text);
+                $("#icon").attr("src", json.current.condition.icon);
+            }
         });
+
     });
 
    });
